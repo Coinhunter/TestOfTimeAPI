3bd4be8aa114521e52655ed17a0ebf7c44b90942
Conrad Ljungstroem <conradl@kth.se>
2015-09-18 21:58:51 +0200

    Adds POST /card route for creating new time-cards

--- README.md
+++ README.md
@@ -14,7 +14,7 @@ You also have to be running MongoDB locally:
 	
 	$ sudo mongod
 
-In order to make calls to the API basic authentication is required. To set up a user with username: `testoftime@time.now` and password: `password` run setup_user.js.
+In order to make calls to the API basic authentication is required. To set up a user with username: `testoftime@time.now` and password: `password` run setup_user.js. This means that the headers should include one `Authorization` entry with value `Basic dGVzdG9mdGltZUB0aW1lLm5vdzpwYXNzd29yZA==`. It also needs to have a `Content-Type` header with `application/json` to work. Note: This is only to set up a user with which to play around.
 	
 	$ node setup_user.js
 
---
+++ setup_cards.js
@@ -0,0 +1,38 @@
+var cardCreator = require('./lib/handlers/card-create.js'),
+	config = require('config'),
+	mongoose = require('mongoose'),
+	CardModel = require('./lib/models/card.js'),
+	db = require('./lib/db.js');
+
+cardCreator.convertParamsToCard(
+{
+	question: 'The RMS Titanic collides with an iceberg and sinks, taking 1500 people with it.',
+	year: 1912
+}, function(err, returned_card){
+	if(err){
+		console.log('Something went wrong: ' + err);
+		process.exit(1);
+	} else {
+		var card = new CardModel(returned_card);
+		db.connect(function(err) {
+			if (err) {
+				console.log('Error connecting to database');
+				if (callback) {
+					callback(err);
+				}
+				process.exit(1);
+			}
+			card.save(function (err, card) {
+				if (err) {
+					console.error(err);
+					process.exit(1);
+				}
+				else {
+					console.log('Card created: ' + card);
+					process.exit();
+				}
+			});
+		});
+	}
+});	
+
